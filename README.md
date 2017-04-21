85e054ed5985e053bd97e0da2ec6453bf00d5b87
alu0100813272 <alu0100813272@ull.edu.es>
2017-04-21 17:36:00 +0000

    Pruebas

---
+++ README.md
@@ -0,0 +1,4 @@
+# creacion-de-paquetes-npm-lucas-ivan-miguel-jairo-triangulo
+
+# Triangulo
+Se ha creado una figura de tipo triangulo 
--- triangulo.js
+++ triangulo.js
@@ -1,15 +1,26 @@
 "use strict"
+
+/*
+     Se require el paquete que contiene la clase Shape.
+*/
 var nuevo = require('ull-shape-lucas-ivan-miguel-jairo');
 var Shape = nuevo.Shape
 
+ /**
+ * Clase para representar un Triángulo que es de tipo Shape.
+ *
+ * @param {object} options Bloque de código.
+ * @param {width} options.width Anchura de la figura.
+ * @param {height} options.height Altura de la figura.
+ */
 class Triangle extends Shape{
     
         constructor(options){
             super(options); 
         }
-          
+        /* @param {function?} done Callback function to execute when finished.*/
         area(){
-           var area = 5 * this.width * this.height;
+           var area = .5 * this.width * this.height;
            return area;
         }
 }
